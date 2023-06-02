284a64d5a6cec6a00d318864f18c4c42449f4c32
Elocodes <elochukwuodoeze@gmail.com>
2023-06-01 20:56:52 -0700

    added 6 funct to header file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -56,8 +56,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	/*else if (tree->left && tree->right)*/
 	return ((binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
-		&& (binary_tree_height(tree->left) == binary_tree_height(tree->right)));
-	/*return (0);*/
+		 && (binary_tree_height(tree->left) == binary_tree_height(tree->right)));
 }
